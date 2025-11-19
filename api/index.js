920a203246f638d8cc8798f06ffeb61f2b3bfde6
AyresGaia <tigermaster3002@gmail.com>
2025-11-20 02:52:00 +0530

    thrid

--- api/index.js
+++ api/index.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var router = express.Router();
+const app = require('../app');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+// Vercel serverless handler
+module.exports = app;
